665e03503f1a62cc4950722c05fb42f570ed90f8
Abhinandan <abhinandan.satpute@devsatwork.in>
2017-09-10 01:13:42 +0530

    UI layout applied 1. Names of areas rendered in select box 2. On selection of area name, associated image has been rendered on UI

--- ui.R
+++ ui.R
@@ -1,3 +1,32 @@
 library(shiny)
+library(shinythemes)
 
-shinyUI(fluidPage()) 
+shinyUI(fluidPage(theme = shinytheme("cerulean"),
+                  
+                  # HTML head section to import css and JS as needed
+                  tags$head(
+                    includeCSS("www/custom_style.css")
+                  ),
+                  
+                  navbarPage(title = "Electoral Roll Mapper", fluid = TRUE, theme = ),
+                  fluidRow(
+                    column(6,
+                           fluidRow(
+                             column(12,
+                                    uiOutput("areas")
+                             )
+                           ),
+                           fluidRow(
+                             column(12,
+                                    div( id = "image_div",
+                                      imageOutput("rough_area_image")
+                                    )
+                             )
+                           )
+                    ),
+                    column(6,
+                           "main"
+                    )
+                  )
+                  
+)) 
