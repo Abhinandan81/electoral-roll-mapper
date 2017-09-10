e60cf1c4536642842bb2d7ecb0900a1ee108ce6d
Abhinandan <abhinandan.satpute@devsatwork.in>
2017-09-10 14:26:45 +0530

    code reformatting: search box id changed

--- www/custom_js.js
+++ www/custom_js.js
@@ -19,7 +19,7 @@
         });
 
         // Create the search box and link it to the UI element.
-        var input = document.getElementById('area-input');
+        var input = document.getElementById('area_input');
         var searchBox = new google.maps.places.SearchBox(input);
         map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
