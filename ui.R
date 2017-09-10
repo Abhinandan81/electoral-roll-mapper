9eda9ead75fd4e921d54cc1e5b4800de978489de
Abhinandan <abhinandan.satpute@devsatwork.in>
2017-09-10 09:55:13 +0530

    Google Map JS integration and map rendering

--- ui.R
+++ ui.R
@@ -1,12 +1,14 @@
 library(shiny)
 library(shinythemes)
+#library(leaflet)
+library(shinyjs)
 
 shinyUI(fluidPage(theme = shinytheme("cerulean"),
                   
                   # HTML head section to import css and JS as needed
                   tags$head(
                     includeCSS("www/custom_style.css")
-                  ),
+                    ),
                   
                   navbarPage(title = "Electoral Roll Mapper", fluid = TRUE, theme = ),
                   fluidRow(
@@ -25,8 +27,25 @@ shinyUI(fluidPage(theme = shinytheme("cerulean"),
                            )
                     ),
                     column(6,
-                           "main"
+                           fluidRow(
+                             column(12,
+                                    "search"
+                             )
+                           ),
+                           fluidRow(
+                             column(12,
+                                    div( id = "map"
+                                         #leafletOutput("map")
+                                    )
+                             )
+                           )
                     )
-                  )
+                  ),
+                  
+                  # ADDING GOOGLE API SCRIPTS
+                  HTML('
+                       <script type="text/javascript" src="custom_js.js"></script>
+                       <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD_TSprWOtIlrzkAgJ7kW4LJj_Z2CoeGHY&callback=initMap" async defer></script>'
+                       )
                   
 )) 
